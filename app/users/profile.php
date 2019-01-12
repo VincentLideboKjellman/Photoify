4a91bc65420c53727b13ab301856dca50460c945
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-01-12 17:20:49 +0100

    Made so you can update your password also edit bio

--- app/users/profile.php
+++ app/users/profile.php
@@ -1,25 +1,28 @@
 <?php
-//create account logic
 declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-if (isset($_POST['name'], $_POST['username'], $_POST['email'])){
+//updates profile info
+if (isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['bio'])){
 
+  $bio = trim(filter_var($_POST['bio'], FILTER_SANITIZE_STRING));
   $name = trim(filter_var($_POST['name'], FILTER_SANITIZE_STRING));
   $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
   $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
 
   $id = $_SESSION['user']['id'];
 
-  $updateStatement = $pdo->prepare('UPDATE users SET name = :name, username = :username, email = :email WHERE id = :id');
+  $updateStatement = $pdo->prepare('UPDATE users SET name = :name, username = :username, email = :email, profile_bio = :bio WHERE id = :id');
+
+  $updateStatement->bindParam(':bio', $bio, PDO::PARAM_STR);
   $updateStatement->bindParam(':id', $id, PDO::PARAM_INT);
   $updateStatement->bindParam(':name', $name, PDO::PARAM_STR);
   $updateStatement->bindParam(':username', $username, PDO::PARAM_STR);
   $updateStatement->bindParam(':email', $email, PDO::PARAM_STR);
   $updateStatement->execute();
 
-
+  $_SESSION['user']['profile_bio'] = $bio;
   $_SESSION['user']['name'] = $name;
   $_SESSION['user']['username'] = $username;
   $_SESSION['user']['email'] = $email;
--- profile.php
+++ profile.php
@@ -1,17 +1,26 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
+<?php if(!isset($_SESSION['user'])){ redirect("/"); } else { $user = $_SESSION['user'];}?>
 <article>
 
   <h1>Profile Info</h1>
   <div class="temp-profie-div">
-    <p>Profile Image:</p><img src="" alt="">
+    <p>Profile Image:</p><img src=<?php $_SESSION['user']['profile_image'] ?> alt="">
     <p>Username: <?php echo $_SESSION['user']['username']; ?></p>
     <p>Name: <?php echo $_SESSION['user']['name']; ?></p>
     <p>Email: <?php echo $_SESSION['user']['email']; ?></p>
+    <p>Bio: <?php echo $_SESSION['user']['profile_bio']; ?></p>
   </div>
 
-    <h1>Edit Account</h1>
-    <form action="app/users/profile.php" method="post">
+
+      <h1>Edit Account info</h1>
+      <form action="app/users/profile.php" method="post">
+        <div class="form-group">
+            <label for="name">Biography</label>
+          <input class="form-control" type="text" name="bio" placeholder="Biography" value=<?php echo $_SESSION['user']['profile_bio'] ?>>
+            <small class="form-text text-muted">Change name</small>
+        </div><!-- /form-group -->
+
         <div class="form-group">
             <label for="name">Name</label>
           <input class="form-control" type="text" name="name" placeholder="Name" value=<?php echo $_SESSION['user']['name'] ?>>
@@ -29,15 +38,26 @@
             <input class="form-control" type="email" name="email" placeholder="Email" value=<?php echo $_SESSION['user']['email'] ?>>
             <small class="form-text text-muted">Change Email</small>
         </div><!-- /form-group -->
+        <button type="submit" class="btn btn-primary">Apply Changes</button>
+    </form>
 
-        <div class="form-group">
+    <h1>Change Password</h1>
+    <form action="app/users/profilePassword.php" method="post">
+      <div class="form-group">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" placeholder="Password">
             <small class="form-text text-muted">Change password</small>
         </div><!-- /form-group -->
-
+        <div class="form-group">
+              <label for="password">Confirm Password</label>
+              <input class="form-control" type="password" name="confirmPassword" placeholder="Confirm Password">
+              <small class="form-text text-muted">Confirm password</small>
+          </div><!-- /form-group -->
         <button type="submit" class="btn btn-primary">Apply Changes</button>
     </form>
 </article>
+
+
+
 
 <?php require __DIR__.'/views/footer.php'; ?>
